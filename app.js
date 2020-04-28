30d1aae2a1ba2d13bf85aaed65241ab334f33241
Suemin Cho <sm991105@gmail.com>
2020-04-28 20:16:57 +0900

    get location from user

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Seoul", (err, data) => {
-  if (err) console.log(err);
-  console.log(data);
-});
+if (process.argv.length !== 3) {
+  console.log("Please provide the right location.");
+} else {
+  const location = process.argv[2];
 
-forecast(37.56, 127, (err, data) => {
-  if (err) console.log(err);
-  console.log(data);
-});
+  geocode(location, (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    forecast(data.latitude, data.longitude, (err, forecastData) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(data.placeName);
+      console.log(forecastData);
+    });
+  });
+}
