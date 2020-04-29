52ea5daca3b4f5eff43cd5ee121721e4f99fd313
Suemin Cho <sm991105@gmail.com>
2020-04-30 00:53:55 +0900

    Client side js

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,3 @@
-/*const geocode = require("./utils/geocode");
-const forecast = require("./utils/forecast");
-
-if (process.argv.length !== 3) {
-  console.log("Please provide the right location.");
-} else {
-  const location = process.argv[2];
-
-  geocode(location, (err, { latitude, longitude, placeName } = {}) => {
-    if (err) {
-      return console.log(err);
-    }
-    forecast(latitude, longitude, (err, forecastData) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(placeName);
-      console.log(forecastData);
-    });
-  });
-}
-*/
-
 console.log("Client side javascript file is loaded!");
 
 const weatherForm = document.querySelector("form");
