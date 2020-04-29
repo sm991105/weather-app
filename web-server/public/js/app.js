85e2353d847eb381655fcd30a82bba15118d1c3c
Suemin Cho <sm991105@gmail.com>
2020-04-30 01:40:53 +0900

    Complete

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,25 @@
-console.log("Client side javascript file is loaded!");
+// Client-side javascript
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
+const msg1 = document.querySelector("#message-1");
+const msg2 = document.querySelector("#message-2");
+
 weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const location = search.value;
-
+  msg1.textContent = "";
+  msg2.textContent = "Loading...";
   fetch(`http://localhost:3000/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.err) {
-          console.log(data.err);
+          msg1.textContent = data.err;
+          msg2.textContent = "";
         } else {
-          console.log(data.address);
-          console.log(data.forecast);
+          msg1.textContent = data.address;
+          msg2.textContent = data.forecast;
         }
       });
     }
